19bb8b3faae8ce4d03df90e80eb3cff9178566b9
plumest <plumest.chayathon@gmail.com>
2022-01-30 22:26:28 +0700

    List restaurants

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddReview from "./components/add-review";
+import Login from "./components/login";
+import RestaurantsList from "./components/restaurants-list";
+import Restaurant from "./components/restaurant";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          <li className="nav-item">
+            {user ? (
+              <a
+                onClick={logout}
+                className="nav-link"
+                style={{ cursor: "pointer" }}
+              >
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path={"/"} element={<RestaurantsList />} />
+          <Route path={"/restaurants"} element={<RestaurantsList />}>
+            <Route path=":id/review" element={<AddReview user={user} />} />
+            <Route path=":id" element={<Restaurant user={user} />} />
+            <Route path="login" element={<Login login={login} />} />
+          </Route>
+          <Route path="login" element={<Login login={login} />} />
+        </Routes>
+      </div>
     </div>
   );
 }
